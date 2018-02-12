11bda00689a9770bf86170169ab1a5924c55acb0
Alexey <marchgridnev96@gmail.com>
2018-02-12 09:59:41 +0300

    Flask App

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+  articles =[
+    {
+      'id' : 1,
+      'title' : 'Title One',
+      'body' : 'Text of first article',
+      'author' : 'John Weak',
+      'create_date' : '09-02-2018'
+    },
+        {
+      'id' : 2,
+      'title' : 'Title Two',
+      'body' : 'Text of second article',
+      'author' : 'John Weak',
+      'create_date' : '09-02-2018'
+    },
+        {
+      'id' : 3,
+      'title' : 'Title Third',
+      'body' : 'Text of third article',
+      'author' : 'John Break',
+      'create_date' : '09-02-2018'
+    }
+  ]
+  return articles
